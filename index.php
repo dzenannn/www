d28eb3b97fc42e4b22c264eead8e9583d33c6403
dzenannn <dzenan.dev@gmail.com>
2025-03-21 15:20:29 +0100

    echo, variables

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>WWW</title>
+  </head>
+  <body>
+    <h1>
+      <?php 
+        $greeting = "Hello Universe. ";
+        $name = "My name is Dženan.";
+        echo $greeting.$name; //? echo = print
+        echo "$greeting This is second line of code.";
+        //! Ako koristimo vrednost varijable unutar stringa koristi se double quote (")
+        //! A ako zelimo da se ispiše IME VARIJABLE koristi se single quote (')
+      ?>
+    </h1>
+  </body>
+</html>
--- site.php
+++ site.php
@@ -9,7 +9,7 @@
 
 <body>
     <?php
-    echo "<h1>Hello World</h1>"
+    echo "<h1>Hello WASDorld</h1>"
     ?>
 </body>
 
