bbf83fc4efb3540465a7a58b14a6a317d9188c1b
dzenannn <dzenan.dev@gmail.com>
2025-03-25 22:24:15 +0100

    PDO Class, database connection, fetching data from db

--- index.php
+++ index.php
@@ -1,5 +1,37 @@
 <?php
 require "./functions.php";
-require "./router.php";
+// require "./router.php";
 
 # I was working with a MySQL Database today :)
+# connect to our MySQL database.
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+class Person {
+    public $name;
+    public $age;
+
+    public function breathe() { #method
+        echo $this->name . " is breathing!";
+    }
+}
+
+$dzeno = new Person();
+
+$dzeno->name = 'Dzenan Mecinovic';
+$dzeno->age = 21;
+
+// $dzeno->breathe();
+
+// dd($posts);
+
+foreach($posts as $post) {
+    echo "<h3>" . $post['id'] . ". " . $post['title'] . "</h3>";
+}
