8e3127c19737a6ff94585b943535bcc7d4e7d5e9
dzenannn <dzenan.dev@gmail.com>
2025-03-31 13:52:02 +0200

    environments and configuration flexibility

--- Database.php
+++ Database.php
@@ -3,9 +3,14 @@
 class Database {
     public $connection;
 
-    public function __construct() {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-        $this->connection = new PDO($dsn);
+    public function __construct($config, $username = 'root', $passsword = '') {
+
+
+        $dsn ='mysql:' . http_build_query($config, '', ';'); // host=localhost;port=3306;dbname=myapp
+
+        $this->connection = new PDO($dsn, $username, $passsword,  [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
     public function query ($query) {
